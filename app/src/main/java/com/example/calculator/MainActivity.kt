6a3c8cd9dd5a26e1c26c0d117bd1c9eb30e6a15c
Gregor <gregorlennie@hotmail.co.uk>
2020-03-15 21:47:47 +0000

    Adds updateCalcDisplay function to change display on button click

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastInput: String = ""
+    var operation: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_cancel.setOnClickListener {
+            clearDisplay()
+        }
+
+        button_num1.setOnClickListener {
+            updateCalcDisplay('1')
+        }
+    }
+
+    private fun updateCalcDisplay(number: Char) {
+        val currentDisplayText: String = calcDisplay.text.toString()
+        if (currentDisplayText == "0") {
+            calcDisplay.text = number.toString()
+        } else {
+            calcDisplay.text = currentDisplayText + number.toString()
+        }
+    }
+
+    private fun clearDisplay() {
+        calcDisplay.text = "0"
     }
 }
